7b6f899f60ea03e6c83d98e5f3d6fd8ea743f4d0
LAPTOP-CR2L7QO4\ilian <ilianamedina03@gmail.com>
2025-03-28 22:09:22 -0700

    Actualizacion

--- test/Rx.m
+++
@@ -1,7 +0,0 @@
-function R = Rx(phi)
-%Rx realiza una rotación en un ángulo theta en radianes con respecto al eje X
-
-R =[1, 0, 0;
-         0, cosd(phi), -sind(phi);
-         0, sind(phi), cosd(phi)];
-end
--- test/Ry.m
+++
@@ -1,7 +0,0 @@
-function R = Ry(theta)
-%Ry realiza una rotación en un ángulo theta en radianes con respecto al eje y
-
-R =[cosd(theta), 0, sind(theta);
-         0, 1, 0;
-         -sind(theta), 0, cosd(theta)];
-end
